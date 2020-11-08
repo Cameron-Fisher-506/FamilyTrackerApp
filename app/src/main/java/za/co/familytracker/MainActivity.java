6672092382e723a347590890989b9790293bbb16
Cameron <cameron@aheadofthecurve.co.za>
2020-11-08 10:42:01 +0200

    Added the ability to update marker location.
    - MainActivity: Added the ability to update marker location.

--- app/src/main/java/za/co/familytracker/MainActivity.java
+++ app/src/main/java/za/co/familytracker/MainActivity.java
@@ -1,8 +1,8 @@
 package za.co.familytracker;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,25 +16,27 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Calendar;
-import java.util.Timer;
-import java.util.TimerTask;
-import za.co.familytracker.za.co.familytracker.utils.ConstantUtils;
+import za.co.familytracker.za.co.familytracker.utils.DTUtils;
 import za.co.familytracker.za.co.familytracker.utils.GeneralUtils;
-import za.co.familytracker.za.co.familytracker.utils.LocationUtils;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener
+{
+    private final String TAG = "MainActivity";
 
     private GoogleMap map;
+    private Marker marker;
 
-    private Handler handler;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setLocationManager();
 
         SupportMapFragment  mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         if(mapFragment != null)
@@ -49,28 +51,56 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     {
         this.map = googleMap;
 
-        LatLng myLocation = new LatLng(-26.127733, 27.982846);
-        this.map.addMarker(new MarkerOptions()
-                .position(myLocation)
-                .title("Me"));
-        this.map.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
-
-        final LocationUtils locationUtils = new LocationUtils(this);
-
-        handler = new Handler();
-
-        handler.postDelayed(new Runnable()
-        {
-            public void run()
-            {
-                if(locationUtils.getCurrentLocation() != null)
-                {
-                    GeneralUtils.makeToast(getApplicationContext(), Double.toString(locationUtils.getCurrentLocation().getLatitude()));
-                }
-
-                handler.postDelayed(this, ConstantUtils.MAP_REFRESH_TIME);
-            }
-        }, ConstantUtils.MAP_REFRESH_TIME);
     }
 
+    private void setLocationManager()
+    {
+        try
+        {
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 5, MainActivity.this);
+        } catch (SecurityException e)
+        {
+            Log.e(TAG, "Error: " + e.getMessage() +
+                    "\nMethod: LocationUtils - startLocationManager" +
+                    "\nCreatedTime: " + DTUtils.getCurrentDateTime());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location != null)
+        {
+            LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(myLocation)
+                    .title("Me");
+
+            if(this.marker == null)
+            {
+                this.marker = this.map.addMarker(markerOptions);
+            }else
+            {
+                marker.setPosition(myLocation);
+            }
+
+            this.map.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
